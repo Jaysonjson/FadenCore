fb2b20a4b9dce5b6aae14f879d5f9bfe1e91db77
Jayson <jaysonjson@web.de>
2024-04-02 23:33:13 +0100

    va

---
+++ src/main/java/json/jayson/common/objects/item/IValue.java
@@ -0,0 +1,9 @@
+package json.jayson.common.objects.item;
+
+import net.minecraft.item.ItemStack;
+
+public interface IValue {
+	
+	int getValue(ItemStack stack);
+	
+}
--- src/main/java/json/jayson/mixin/ItemMixin.java
+++ src/main/java/json/jayson/mixin/ItemMixin.java
@@ -1,6 +1,14 @@
 package json.jayson.mixin;
 
 
+import java.util.Optional;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import json.jayson.common.objects.item.IValue;
 import json.jayson.common.objects.tooltip.ItemValueTooltipComponent;
 import json.jayson.common.objects.tooltip.ItemValueTooltipData;
 import json.jayson.data.ItemValues;
@@ -8,21 +16,14 @@ import net.minecraft.client.item.TooltipData;
 import net.minecraft.item.BundleItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Optional;
-import java.util.Random;
 
 @Mixin(Item.class)
-public class ItemMixin {
+public class ItemMixin implements IValue {
 
 
     @Inject(at = @At("HEAD"), method = "getTooltipData", cancellable = true)
     private void getTooltipData(ItemStack stack, CallbackInfoReturnable<Optional<TooltipData>> cir) {
-        int value = ItemValues.VALUES.getOrDefault(stack.getItem(), 0);
+        int value = getValue(stack);
         if(value != 0) {
             if (cir.getReturnValue() == null && !(stack.getItem() instanceof BundleItem)) {
                 cir.setReturnValue(Optional.of(new ItemValueTooltipData(ItemValueTooltipComponent.generateMap(value))));
@@ -31,5 +32,12 @@ public class ItemMixin {
             }
         }
     }
+
+	@Override
+	public int getValue(ItemStack stack) {
+		int initialValue = ItemValues.VALUES.getOrDefault(stack.getItem(), 0);
+		//TODO: OTHER CALCULATIONS HERE
+		return initialValue;
+	}
 
 }
