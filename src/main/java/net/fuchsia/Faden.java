4f9af8346f56dbdcc4984bd4d70a897decc5e00b
Jayson <jaysonjson@web.de>
2024-04-17 13:47:26 +0100

    f

--- src/main/java/net/fuchsia/Faden.java
+++ src/main/java/net/fuchsia/Faden.java
@@ -1,6 +1,6 @@
 package net.fuchsia;
 
-import net.fuchsia.common.init.FadenSoundEvents;
+import net.fuchsia.common.init.*;
 import net.fuchsia.config.FadenConfigScreen;
 import net.minecraft.client.gui.screen.Screen;
 import org.slf4j.Logger;
@@ -13,8 +13,6 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.fuchsia.common.objects.command.FadenCommands;
-import net.fuchsia.common.init.FadenItems;
-import net.fuchsia.common.init.FadenTabs;
 import net.fuchsia.common.objects.CoinMap;
 import net.fuchsia.common.data.ItemValues;
 import net.fuchsia.network.FadenNetwork;
@@ -35,7 +33,7 @@ public class Faden implements ModInitializer {
 		FadenSoundEvents.register();
 		FadenItems.register();
 		FadenTabs.register();
-		CoinMap.reloadCoins();
+		CoinMap.reloadMap();
 		FadenNetwork.registerC2S();
 		ItemValues.add();
 		CommandRegistrationCallback.EVENT.register(new FadenCommands());
@@ -52,12 +50,7 @@ public class Faden implements ModInitializer {
 		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
 			ServerPlayerEntity serverPlayerEntity = handler.getPlayer();
 			FadenNetwork.Server.sendAllRaces(serverPlayerEntity);
-			String id = RaceSkinMap.Cache.getId(serverPlayerEntity.getUuid());
-			if(!id.isEmpty()) {
-				for (ServerPlayerEntity playerEntity : server.getPlayerManager().getPlayerList()) {
-					FadenNetwork.Server.sendRaceSkin(playerEntity, serverPlayerEntity.getUuid(), id);
-				}
-			}
+			RaceSkinMap.Cache.sendUpdate(serverPlayerEntity, server);
 		});
 
 
