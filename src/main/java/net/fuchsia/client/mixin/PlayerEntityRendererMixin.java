c5c10df8387ae29bd39a8ae9f59e27c54e58faf1
Jayson <jaysonjson@web.de>
2024-04-19 00:05:00 +0100

    add custom chest render feature

--- src/main/java/net/fuchsia/client/mixin/PlayerEntityRendererMixin.java
+++ src/main/java/net/fuchsia/client/mixin/PlayerEntityRendererMixin.java
@@ -1,20 +1,30 @@
 package net.fuchsia.client.mixin;
 
+import net.fuchsia.client.render.feature.ChestFeatureRenderer;
 import net.fuchsia.config.FadenConfig;
 import net.fuchsia.common.race.skin.client.ClientRaceSkinCache;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntityRenderer.class)
 public abstract class PlayerEntityRendererMixin {
+    private PlayerEntityRenderer renderer = ((PlayerEntityRenderer) ((Object) this));
+
+    @Inject(method = "<init>*", at = @At("TAIL"))
+    public void constructorHead(EntityRendererFactory.Context ctx, boolean slim, CallbackInfo ci) {
+        renderer.addFeature(new ChestFeatureRenderer(renderer));
+    }
 
     @Inject(at = @At("HEAD"), method = "getTexture(Lnet/minecraft/client/network/AbstractClientPlayerEntity;)Lnet/minecraft/util/Identifier;", cancellable = true)
     private void getTextureAbstractPlayer(AbstractClientPlayerEntity abstractClientPlayerEntity, CallbackInfoReturnable<Identifier> cir) {
---
+++ src/main/java/net/fuchsia/client/render/feature/ChestFeatureRenderer.java
@@ -0,0 +1,19 @@
+package net.fuchsia.client.render.feature;
+
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+
+public class ChestFeatureRenderer extends FeatureRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
+    public ChestFeatureRenderer(FeatureRendererContext<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> context) {
+        super(context);
+    }
+
+    @Override
+    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, AbstractClientPlayerEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
+
+    }
+}
