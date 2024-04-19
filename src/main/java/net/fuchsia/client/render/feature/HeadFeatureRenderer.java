418ee237a3c5cfb81485f455b2b86589ba44cafd
Jayson <jaysonjson@web.de>
2024-04-19 01:02:26 +0100

    AAAAAAAAAAAAAAA

--- src/main/java/net/fuchsia/client/render/feature/HeadFeatureRenderer.java
+++ src/main/java/net/fuchsia/client/render/feature/HeadFeatureRenderer.java
@@ -2,12 +2,14 @@ package net.fuchsia.client.render.feature;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.ModelWithHead;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.client.render.model.BakedModel;
@@ -20,6 +22,7 @@ import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.RotationAxis;
+import org.joml.Math;
 import org.joml.Quaternionf;
 
 import java.util.Iterator;
@@ -35,36 +38,32 @@ public class HeadFeatureRenderer extends FeatureRenderer<AbstractClientPlayerEnt
     public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, AbstractClientPlayerEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
         matrices.push();
         BakedModel model = MinecraftClient.getInstance().getBakedModelManager().getModel(new ModelIdentifier(new Identifier("faden", "bunny_ears"), "inventory"));
-        matrices.translate(-model.getTransformation().head.translation.x, -model.getTransformation().head.translation.y, -model.getTransformation().head.translation.z);
-        matrices.scale(model.getTransformation().head.scale.x, model.getTransformation().head.scale.y, model.getTransformation().head.scale.z);
-        //matrices.multiply(RotationAxis.of(model.getTransformation().head.rotation).rotation(360));
-        //matrices.multiply(RotationAxis.POSITIVE_Z.rotation(45));
+        //matrices.translate(-model.getTransformation().head.translation.x, -model.getTransformation().head.translation.y, -model.getTransformation().head.translation.z);
+        //matrices.scale(model.getTransformation().head.scale.x, model.getTransformation().head.scale.y, model.getTransformation().head.scale.z);
+        //matrices.multiply(RotationAxis.of(model.getTransformation().head.rotation).rotationDegrees(0));
         ((ModelWithHead)this.getContextModel()).getHead().rotate(matrices);
-        MinecraftClient.getInstance().getItemRenderer().renderItem(new ItemStack(Items.GRASS_BLOCK), ModelTransformationMode.NONE, false, matrices, vertexConsumers,light, light, model);
-
+        
         net.minecraft.util.math.random.Random random = net.minecraft.util.math.random.Random.create();
         Direction[] var10 = Direction.values();
 
         for (Direction direction : var10) {
             random.setSeed(42L);
-            renderModel(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityGlint()), model.getQuads(null, direction, random), light);
+            renderModel(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityCutout(new Identifier("test", "test"))), model.getQuads(null, direction, random), light);
         }
 
         random.setSeed(42L);
-        renderModel(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityGlint()), model.getQuads(null, null, random), light);
+        renderModel(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityCutout(new Identifier("test", "test"))), model.getQuads(null, null, random), light);
         matrices.pop();
     }
 
     public static void renderModel(MatrixStack matrices, VertexConsumer vertices, List<BakedQuad> quads, int light) {
         MatrixStack.Entry entry = matrices.peek();
-        Iterator var9 = quads.iterator();
 
-        while(var9.hasNext()) {
-            BakedQuad bakedQuad = (BakedQuad)var9.next();
+        for (BakedQuad bakedQuad : quads) {
             int i = bakedQuad.hasColor() ? bakedQuad.getColorIndex() : -1;
-            float f = (float)(i >> 16 & 255) / 255.0F;
-            float g = (float)(i >> 8 & 255) / 255.0F;
-            float h = (float)(i & 255) / 255.0F;
+            float f = (float) (i >> 16 & 255) / 255.0F;
+            float g = (float) (i >> 8 & 255) / 255.0F;
+            float h = (float) (i & 255) / 255.0F;
             vertices.quad(entry, bakedQuad, f, g, h, light, light);
         }
     }
