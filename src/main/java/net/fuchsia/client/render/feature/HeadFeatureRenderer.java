5453323adf8407b491d86c9d773c4727fb664ecb
Jayson <jaysonjson@web.de>
2024-04-19 19:03:09 +0100

    Update HeadFeatureRenderer.java

--- src/main/java/net/fuchsia/client/render/feature/HeadFeatureRenderer.java
+++ src/main/java/net/fuchsia/client/render/feature/HeadFeatureRenderer.java
@@ -24,19 +24,21 @@ public class HeadFeatureRenderer extends FeatureRenderer<AbstractClientPlayerEnt
 
     @Override
     public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, AbstractClientPlayerEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
-        RaceData raceData = ClientRaceCache.get(entity.getUuid());
-        if(raceData.getRace() != null) {
-            for (RaceCosmetic cosmetic : raceData.getRace().getCosmeticPalette().getCosmetics(raceData.getSubId())) {
-                if(cosmetic.getType() == RaceCosmeticType.HEAD) {
-                    matrices.push();
-                    BakedModel model = MinecraftClient.getInstance().getBakedModelManager().getModel(cosmetic.getModel());
-                    ((ModelWithHead) this.getContextModel()).getHead().rotate(matrices);
-                    Transformation head = model.getTransformation().head;
-                    matrices.translate(-head.translation.x + 0.63, -head.translation.y - 0.42f, -head.translation.z - 1);
-                    matrices.multiply(RotationAxis.of(model.getTransformation().head.rotation).rotationDegrees(0));
-                    matrices.scale(head.scale.x - 0.5f, head.scale.y - 0.5f, head.scale.z - 0.5f);
-                    matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(180));
-                    FadenRenderUtil.renderBakedModel(matrices, vertexConsumers, model, light);
+        if(!entity.isInvisible()) {
+            RaceData raceData = ClientRaceCache.get(entity.getUuid());
+            if (raceData.getRace() != null) {
+                for (RaceCosmetic cosmetic : raceData.getRace().getCosmeticPalette().getCosmetics(raceData.getSubId())) {
+                    if (cosmetic.getType() == RaceCosmeticType.HEAD) {
+                        matrices.push();
+                        BakedModel model = MinecraftClient.getInstance().getBakedModelManager().getModel(cosmetic.getModel());
+                        ((ModelWithHead) this.getContextModel()).getHead().rotate(matrices);
+                        Transformation head = model.getTransformation().head;
+                        matrices.translate(-head.translation.x + 0.63, -head.translation.y - 0.42f, -head.translation.z - 1);
+                        matrices.multiply(RotationAxis.of(model.getTransformation().head.rotation).rotationDegrees(0));
+                        matrices.scale(head.scale.x - 0.5f, head.scale.y - 0.5f, head.scale.z - 0.5f);
+                        matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(180));
+                        FadenRenderUtil.renderBakedModel(matrices, vertexConsumers, model, light);
+                    }
                 }
             }
         }
