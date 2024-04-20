33a44dac84fabcfd49896d0dce8bbc503638c68f
Jayson <jaysonjson@web.de>
2024-04-20 16:43:04 +0100

    Update CurrencyCommand.java

--- src/main/java/net/fuchsia/common/objects/command/CurrencyCommand.java
+++ src/main/java/net/fuchsia/common/objects/command/CurrencyCommand.java
@@ -20,7 +20,7 @@ public class CurrencyCommand {
                 .then(CommandManager.literal("currency").requires((source) -> source.hasPermissionLevel(2))
                 .then(CommandManager.literal("give")
                         .then(CommandManager.argument("player", EntityArgumentType.player())
-                                .then(CommandManager.argument("amount", IntegerArgumentType.integer(0, 99999))
+                                .then(CommandManager.argument("amount", IntegerArgumentType.integer(0, 9999999))
                                         .executes(context -> giveCurrency(context)))))
 
                 .then(CommandManager.literal("remove")
