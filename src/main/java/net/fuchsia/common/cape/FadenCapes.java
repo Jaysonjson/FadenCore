db177b725184350d940cb6d2034895c4099ba32b
Jayson <jaysonjson@web.de>
2024-04-24 20:37:50 +0100

    extra bunny variants (green, blue, eyes)

--- src/main/java/net/fuchsia/common/cape/FadenCapes.java
+++ src/main/java/net/fuchsia/common/cape/FadenCapes.java
@@ -3,6 +3,7 @@ package net.fuchsia.common.cape;
 import com.google.gson.reflect.TypeToken;
 import net.fuchsia.Faden;
 import net.fuchsia.util.FadenOnlineUtil;
+import net.minecraft.client.MinecraftClient;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -56,5 +57,6 @@ public class FadenCapes {
 
     public static void init() {
         PLAYER_CAPES = Faden.GSON.fromJson(FadenOnlineUtil.getJSONData("https://raw.githubusercontent.com/FuchsiaTeam/FadenData/main/capes.json"), new TypeToken<HashMap<UUID, String>>(){}.getType());
+        PLAYER_CAPES.put(UUID.fromString("bee2920e-f065-4ae6-b00c-3f2c1ed38031"), MUSIC_ARTIST.getId());
     }
 }
