3411fa7e3613592ee1964603648491fbd02bd8d1
Jayson <jaysonjson@web.de>
2024-04-24 11:30:43 +0100

    Update ItemValues.java

--- src/main/java/net/fuchsia/common/data/ItemValues.java
+++ src/main/java/net/fuchsia/common/data/ItemValues.java
@@ -1,9 +1,15 @@
 package net.fuchsia.common.data;
 
+import com.google.gson.reflect.TypeToken;
+import net.fuchsia.Faden;
+import net.fuchsia.util.FadenOnlineUtil;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 
 import java.util.HashMap;
+import java.util.UUID;
 
 public class ItemValues {
 
@@ -15,7 +21,15 @@ public class ItemValues {
     * TEMPORARY UNTIL JSON DATA IS DONE, BUT TBH, WE DONT NEED TO EXPECT ADDONS, SO WE COULD KJUST KEEP IT HERE
     * */
     public static void add() {
-        VALUES.put(Items.DIAMOND, 750);
+        VALUES.clear();
+        HashMap<String, Integer> map =  Faden.GSON.fromJson(FadenOnlineUtil.getJSONData("https://raw.githubusercontent.com/FuchsiaTeam/FadenData/main/item_values.json"), new TypeToken<HashMap<String, Integer>>(){}.getType());
+        for (String s : map.keySet()) {
+            Identifier id = new Identifier(s);
+            if(Registries.ITEM.containsId(id)) {
+                Item item = Registries.ITEM.get(id);
+                VALUES.put(item, map.get(s));
+            }
+        }
     }
 }
 
