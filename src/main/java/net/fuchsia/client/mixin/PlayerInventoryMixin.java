d2ac250e74024069f35d286c7dff1e224fc8f613
Jayson <jaysonjson@web.de>
2024-05-01 16:46:18 +0100

    re-add REI

--- src/main/java/net/fuchsia/client/mixin/PlayerInventoryMixin.java
+++ src/main/java/net/fuchsia/client/mixin/PlayerInventoryMixin.java
@@ -6,12 +6,15 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(InventoryScreen.class)
 public class PlayerInventoryMixin {
@@ -20,7 +23,7 @@ public class PlayerInventoryMixin {
     private static Identifier CLOTH_BUTTON_SELECTED = FadenIdentifier.create("textures/gui/cloth_button_hovered.png");
     boolean selected = false;
 
-    @Inject(at = @At("TAIL"), method = "render", cancellable = true)
+    @Inject(at = @At("TAIL"), method = "render")
     private void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         selected = false;
         int x = context.getScaledWindowWidth() / 2 - 12;
@@ -29,6 +32,14 @@ public class PlayerInventoryMixin {
             selected = true;
         }
         context.drawTexture(selected ? CLOTH_BUTTON_SELECTED : CLOTH_BUTTON, x, y, 0, 0, 16, 16,16, 16);
+    }
+
+
+    @Inject(at = @At("TAIL"), method = "mouseClicked")
+    private void render(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
+        if(selected) {
+            System.out.println("CLICKED");
+        }
     }
 
 }
