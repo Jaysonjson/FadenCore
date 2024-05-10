87ad57cb8047e4796b0c9372257e1a575c27561e
Jayson <jaysonjson@web.de>
2024-05-10 03:49:23 +0100

    f

--- src/main/java/net/fuchsia/common/cape/online/OnlineCapes.java
+++ src/main/java/net/fuchsia/common/cape/online/OnlineCapes.java
@@ -5,12 +5,10 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.fuchsia.Faden;
 import net.fuchsia.common.cape.FadenCapes;
 import net.fuchsia.config.FadenOptions;
+import net.fuchsia.util.FadenCheckSum;
 import net.fuchsia.util.FadenOnlineUtil;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,21 +17,29 @@ import java.util.UUID;
 
 public class OnlineCapes {
 
-	private static final Path CACHE_PATH = new File(FabricLoader.getInstance().getGameDir().toString() + "/faden/cache/" + Faden.MC_VERSION + "/online_capes.json").toPath();
+	private static final Path CACHE_PATH = new File(FabricLoader.getInstance().getGameDir().toString() + "/faden/cache/client/online_capes.json").toPath();
 	public static OnlineCapeCache ONLINE_CAPES = new OnlineCapeCache();
 
 	public static void retrieve()  {
 		if(FadenOptions.getConfig().CUSTOM_CAPES) {
 			try {
 				File cache = CACHE_PATH.toFile();
+				cache.mkdirs();
 				if(!cache.exists()) {
 					ONLINE_CAPES = Faden.GSON.fromJson(FadenOnlineUtil.getJSONData("https://raw.githubusercontent.com/FuchsiaTeam/FadenData/main/online_capes.json"), OnlineCapeCache.class);
+					FileWriter writer = new FileWriter(cache);
+					writer.write(Faden.GSON.toJson(ONLINE_CAPES));
+					writer.close();
 				} else {
 					InputStream inputStream = new FileInputStream(cache);
 					ONLINE_CAPES = Faden.GSON.fromJson(new FileReader(cache), OnlineCapeCache.class);
-					if(!ONLINE_CAPES.checksum.equals(Faden.CHECKSUMS.ONLINE_CAPES)) {
+					if(!FadenCheckSum.checkSum(inputStream).equals(Faden.CHECKSUMS.ONLINE_CAPES)) {
+						Faden.LOGGER.warn("Mismatched Checksum for " + cache + " - retrieving data again");
 						ONLINE_CAPES = Faden.GSON.fromJson(FadenOnlineUtil.getJSONData("https://raw.githubusercontent.com/FuchsiaTeam/FadenData/main/online_capes.json"), OnlineCapeCache.class);
 					}
+					FileWriter writer = new FileWriter(cache);
+					writer.write(Faden.GSON.toJson(ONLINE_CAPES));
+					writer.close();
 				}
 
 				for (OnlineCape onlineCape : ONLINE_CAPES.CAPES) {
