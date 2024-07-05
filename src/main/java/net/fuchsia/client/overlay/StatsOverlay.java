8793afc2a6dce1fb2acc35b88629e4a443fd8f69
Jayson <jaysonjson@web.de>
2024-07-05 17:32:44 +0200

    fix crash

--- src/main/java/net/fuchsia/client/overlay/StatsOverlay.java
+++ src/main/java/net/fuchsia/client/overlay/StatsOverlay.java
@@ -23,7 +23,7 @@ public class StatsOverlay implements HudRenderCallback {
             ClientPlayerEntity player = MinecraftClient.getInstance().player;
             RaceData data = ClientRaceCache.getCache().getOrDefault(player.getUuid(), null);
             if(data != null && data.getRace() != null) {
-                drawContext.drawGuiTexture(data.getRace().getIcon(), 25, 10, 16, 16);
+                //drawContext.drawGuiTexture(data.getRace().getIcon(), 25, 10, 16, 16);
             }
 
             drawBar(drawContext, (int) player.getHealth() * 2, (int) player.getMaxHealth() * 2, 0xABe35146, 15);
@@ -37,16 +37,5 @@ public class StatsOverlay implements HudRenderCallback {
     public void drawBar(DrawContext drawContext, int current, int max, int color, int y) {
         drawContext.fill(25, y, 25 + max, y + 3, 0xAB454545);
         drawContext.fill(25, y, 25 + current, y + 3, color);
-    }
-
-    public void renderBlur(float delta) {
-        float g = 50f;
-        if (MinecraftClient.getInstance().gameRenderer.blurPostProcessor != null && g >= 1.0F) {
-            RenderSystem.enableBlend();
-            MinecraftClient.getInstance().gameRenderer.blurPostProcessor.setUniforms("Radius", g);
-            MinecraftClient.getInstance().gameRenderer.blurPostProcessor.render(delta);
-            RenderSystem.disableBlend();
-        }
-        MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
     }
 }
